3cd1efbbf62582a4815a62e6b53b7fd4640c6bbe
Tania Ali <taniafali@gmail.com>
2024-01-07 22:08:28 -0800

    Add solution to the form challenge

--- controlled-forms-react/src/main.jsx
+++ controlled-forms-react/src/main.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Form from './Form';
+import SignupForm from './SignupForm';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Form />
+    <hr/>
+    <SignupForm/>
   </React.StrictMode>,
 )
